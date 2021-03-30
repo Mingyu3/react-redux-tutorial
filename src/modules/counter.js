bb92788fbdb1ed3a3e6d13fe3400f5d1bf986672
Mingyu <aeneascon@gmail.com>
2021-03-30 23:16:44 +0900

    commit 17.3

---
+++ src/modules/counter.js
@@ -0,0 +1,26 @@
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+const inintialState = {
+  number: 0,
+};
+
+function counter(state = inintialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
